63de09e0a75c5b72369232b021275ca775a928c8
Glhazyanno Linhares Braga <glbraga9@gmail.com>
2022-04-15 21:31:08 -0300

    Projetar-lista-do-JS

---
+++ modulo1/projeto-lista-js/exercicios.js
@@ -0,0 +1,155 @@
+// EXEMPLOS DE IMPLEMENTAÇÃO ---------------------------------------------------------------
+
+// EXERCÍCIO 0A
+function soma(num1, num2) {
+  // implemente sua lógica aqui
+  return num1 + num2
+}
+
+// EXERCÍCIO 0B
+function imprimeMensagem() {
+  // implemente sua lógica aqui
+  const mensagem = prompt('Digite uma mensagem!')
+
+  console.log(mensagem)
+}
+
+// EXERCÍCIOS PARA FAZER ------------------------------------------------------------------
+
+// EXERCÍCIO 01
+function calculaAreaRetangulo() {
+  // implemente sua lógica aquis
+let altura = prompt("Digite a altura:")
+    altura = Number(altura)
+let largura = prompt("Digite a largura:")
+    largura = Number(largura)
+
+let areaRetangulo = altura * largura 
+console.log(areaRetangulo)
+
+}
+
+// EXERCÍCIO 02
+function imprimeIdade() {
+  // implemente sua lógica aqui
+
+let anoAtual = prompt("Digite o ano atual:")
+    anoAtual = Number(anoAtual)
+     
+let anoNasc = prompt("Digite o ano atual:")
+    anoNasc = Number(anoNasc)
+     
+     
+     
+let idadeAtual = anoAtual - anoNasc
+console.log(idadeAtual)
+
+} 
+
+// EXERCÍCIO 03
+/*function calculaIMC(peso, altura) {
+  // implemente sua lógica aqui
+
+let peso = prompt("Digite o peso:")
+    peso = Number(peso)
+   
+let altura = prompt("Digite a altura:")
+    altura = Number(altura)
+   
+   
+   
+// let calImc = peso / (altura * altura)
+let calImc = 8
+return (calImc)
+
+}*/
+
+// EXERCÍCIO 04
+function imprimeInformacoesUsuario() {
+  // implemente sua lógica aqui
+  // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+
+let NOME = prompt("Digite seu nome")
+let IDADE = prompt("Digite seu idade")
+let EMAIL = prompt("Digite seu email")
+
+console.log("Meu nome é ",NOME,"tenho" ,IDADE, " anos, e o meu email é ",EMAIL,)
+
+}
+
+// EXERCÍCIO 05
+function imprimeTresCoresFavoritas() {
+  // implemente sua lógica aqui
+
+  let cor1 = prompt("Digite a cor1")
+  let cor2 = prompt("Digite a cor2")
+  let cor3 = prompt("Digite a cor3")
+
+cor1 = "Azul"
+cor2 = "Amarelo"
+cor3 = "Vermelho"
+
+
+return console.log (cor1,cor2,cor3, )
+
+}
+
+// EXERCÍCIO 06
+function retornaStringEmMaiuscula(string) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 07
+function calculaIngressosEspetaculo(custo, valorIngresso) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 08
+function checaStringsMesmoTamanho(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 09
+function retornaPrimeiroElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 10
+function retornaUltimoElemento(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 11
+function trocaPrimeiroEUltimo(array) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 12
+function checaIgualdadeDesconsiderandoCase(string1, string2) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 13
+function checaRenovacaoRG() {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 14
+function checaAnoBissexto(ano) {
+  // implemente sua lógica aqui
+
+}
+
+// EXERCÍCIO 15
+function checaValidadeInscricaoLabenu() {
+  // implemente sua lógica aqui
+
+}
