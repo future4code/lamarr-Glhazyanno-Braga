5d3f957b9aa3a90e08895b2be3027766fcbc66be
Glhazyanno Linhares Braga <glbraga9@gmail.com>
2022-04-24 11:53:00 -0300

    Projeto de lista atualizado

--- modulo1/projeto-lista-js/exercicios.js
+++ modulo1/projeto-lista-js/exercicios.js
@@ -18,62 +18,59 @@ function imprimeMensagem() {
 
 // EXERCÍCIO 01
 function calculaAreaRetangulo() {
-  // implemente sua lógica aquis
-let altura = prompt("Digite a altura:")
-    altura = Number(altura)
-let largura = prompt("Digite a largura:")
-    largura = Number(largura)
+  // implemente sua lógica aqui
+  let  altura  =  prompt ( "Digite a altura:" )
+       altura  =  Number(altura)
+let  largura  =  prompt ( "Digite a largura:" )
+     largura  =  Number(largura)
 
-let areaRetangulo = altura * largura 
-console.log(areaRetangulo)
+const  areaRetangulo  =  altura  *  largura 
+return console.log(areaRetangulo)
 
+ 
 }
 
 // EXERCÍCIO 02
 function imprimeIdade() {
   // implemente sua lógica aqui
+  let  anoAtual  =  prompt ("Digite o ano atual:")
+       anoAtual  =  Number(anoAtual)
 
-let anoAtual = prompt("Digite o ano atual:")
-    anoAtual = Number(anoAtual)
-     
-let anoNasc = prompt("Digite o ano atual:")
-    anoNasc = Number(anoNasc)
-     
-     
-     
-let idadeAtual = anoAtual - anoNasc
-console.log(idadeAtual)
+let    anoNasc  =  prompt ("Digite o ano atual:")
+       anoNasc  =  Number(anoNasc)
 
-} 
+
+
+const  idadeAtual  =  anoAtual  -  anoNasc
+return console.log (idadeAtual)
+
+}
 
 // EXERCÍCIO 03
-/*function calculaIMC(peso, altura) {
+function calculaIMC(peso, altura) {
   // implemente sua lógica aqui
+ peso = Number(peso)
+ altura = Number(altura)
+   let  al = altura * altura  
+   let calImc = peso / al
+ 
+return calImc
 
-let peso = prompt("Digite o peso:")
-    peso = Number(peso)
-   
-let altura = prompt("Digite a altura:")
-    altura = Number(altura)
-   
-   
-   
-// let calImc = peso / (altura * altura)
-let calImc = 8
-return (calImc)
 
-}*/
+}
 
 // EXERCÍCIO 04
 function imprimeInformacoesUsuario() {
   // implemente sua lógica aqui
   // "Meu nome é NOME, tenho IDADE anos, e o meu email é EMAIL."
+ let nome = prompt('Diga seu nome')
+ let idade = prompt ('Diga sua idade')
+ let email = prompt ('Diga seu email')
 
-let NOME = prompt("Digite seu nome")
-let IDADE = prompt("Digite seu idade")
-let EMAIL = prompt("Digite seu email")
 
-console.log("Meu nome é ",NOME,"tenho" ,IDADE, " anos, e o meu email é ",EMAIL,)
+ let a = console.log(`Meu nome é ${nome}, tenho ${idade} anos, e o meu email é ${email}.`)
+ return a
+
 
 }
 
@@ -81,70 +78,102 @@ console.log("Meu nome é ",NOME,"tenho" ,IDADE, " anos, e o meu email é ",EMAIL
 function imprimeTresCoresFavoritas() {
   // implemente sua lógica aqui
 
-  let cor1 = prompt("Digite a cor1")
-  let cor2 = prompt("Digite a cor2")
-  let cor3 = prompt("Digite a cor3")
+  let cor1 = prompt('Diga sua Primeira cor favorita')
+  let cor2 = prompt ('Diga sua Segunda cor favorita')
+  let cor3 = prompt ('Diga sua Terceira cor favorita')
 
-cor1 = "Azul"
-cor2 = "Amarelo"
-cor3 = "Vermelho"
-
-
-return console.log (cor1,cor2,cor3, )
+const listaCor = [cor1 , cor2 , cor3]
+const imprimeLista = console.log(listaCor)
+return imprimeLista
 
 }
 
 // EXERCÍCIO 06
 function retornaStringEmMaiuscula(string) {
   // implemente sua lógica aqui
-
+ let letramaiuscula = string.toUpperCase()
+ 
+ return letramaiuscula
 }
 
 // EXERCÍCIO 07
 function calculaIngressosEspetaculo(custo, valorIngresso) {
   // implemente sua lógica aqui
 
+  let sempreju = custo / valorIngresso
+  return sempreju
 }
 
 // EXERCÍCIO 08
 function checaStringsMesmoTamanho(string1, string2) {
   // implemente sua lógica aqui
 
+  
+  
+  let compara = string1.length === string2.length
+  return compara
+
 }
 
 // EXERCÍCIO 09
 function retornaPrimeiroElemento(array) {
   // implemente sua lógica aqui
-
+let retornaUm = array[0]
+return retornaUm
 }
 
 // EXERCÍCIO 10
 function retornaUltimoElemento(array) {
   // implemente sua lógica aqui
+let retornaUltimo = array.pop()
+return retornaUltimo
 
 }
 
 // EXERCÍCIO 11
 function trocaPrimeiroEUltimo(array) {
   // implemente sua lógica aqui
+  let primeiroArray = array[0]
+      array.slice(0,1)
+  let ultimoArray = array.pop()
+      array.push(primeiroArray)
+      array[0] = ultimoArray
+
+   
+  return array
 
 }
 
 // EXERCÍCIO 12
 function checaIgualdadeDesconsiderandoCase(string1, string2) {
   // implemente sua lógica aqui
+  let frase1 = string1.toUpperCase()
+  let frase2 = string2.toUpperCase()
 
+  let compara = frase1 === frase2
+
+  return compara
 }
 
 // EXERCÍCIO 13
 function checaRenovacaoRG() {
   // implemente sua lógica aqui
+ /* let anoAtual = prompt('Diga o ano atual')
+  let anoNascUsuario = prompt ('Diga o ano de nascimento do usaurio')
+  let anoEmissao = prompt ('Diga o ano de Emissão da carteira')
+
+  let idade = anoAtual - anoNascUsuario
+  
+  idade <= 
+
+*/
 
 }
 
 // EXERCÍCIO 14
 function checaAnoBissexto(ano) {
   // implemente sua lógica aqui
+
 
 }
 
