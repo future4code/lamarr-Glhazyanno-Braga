4f218915913a2905e217d7a5557338ba144505a1
Glhazyanno Linhares Braga <glbraga9@gmail.com>
2022-06-29 16:24:38 -0300

    erro-no-curtir

---
+++ modulo3/estados-insta4/src/components/Post/Post.js
@@ -0,0 +1,88 @@
+import React, {useState} from 'react'
+import './style.css'
+
+import {IconeComContador} from '../IconeComContador/IconeComContador'
+import iconeCoracaoBranco from '../../img/favorite-white.svg'
+import iconeCoracaoPreto from '../../img/favorite.svg'
+import iconeComentario from '../../img/comment_icon.svg'
+import {SecaoComentario} from '../SecaoComentario/SecaoComentario'
+import {SecaoCurtir} from '../SecaoComentario/SecaoCurtir'
+
+
+function Post(props){
+  
+
+  const [numeroCurtidas, setnumeroCurtidas] = useState (0)
+  const [curtido, setCurtido] = useState(false)
+  const [comentando, setComentando] = useState(false)
+  const [numeroComentarios, setNumeroComentarios] = useState(0)
+
+  /*const onClickCurtida = () => {
+    setCurtido(!curtido)
+    if(curtido) {
+      componenteCurtido = <SecaoCurtir aoEnviar={aoEnviarCurtir}/>
+    }
+    console.log(curtir)
+  }*/
+  
+  const onClickComentario = () => {
+    setComentando(!comentando)
+    if(comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario}/>
+    }
+    console.log(comentando)
+  }
+  
+  const aoEnviarCurtir = () => {
+    setCurtido(false)
+    setnumeroCurtidas(numeroCurtidas + 1)
+  }
+  
+  const aoEnviarComentario = () => {
+    setComentando(false)
+    setNumeroComentarios(numeroComentarios + 1)
+  }
+ 
+  let iconeCurtida
+
+    if(curtido) {
+      iconeCurtida = iconeCoracaoPreto
+    } else {
+      iconeCurtida = iconeCoracaoBranco
+    }
+
+    let componenteComentario
+
+    if(comentando) {
+      componenteComentario = <SecaoComentario aoEnviar={aoEnviarComentario}/>
+    }
+
+  return(
+    <div className = 'PostContainer'>
+      <div className = 'PostHeader'>
+        <img className = 'UserPhoto' src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <p>{props.nomeUsuario}</p>
+      </div>
+
+      <img className = 'PostPhoto'src={props.fotoPost} alt={'Imagem do post'}/>
+
+      <div className = 'PostFooter'>
+        <IconeComContador
+          icone={iconeCurtida}
+          onClickIcone={onClickCurtida}
+          valorContador={numeroCurtidas}
+        />
+
+        <IconeComContador
+          icone={iconeComentario}
+          onClickIcone={onClickComentario}
+          valorContador={numeroComentarios}
+        />
+      </div>
+      {componenteComentario}
+    </div>
+  )
+}
+
+
+export default Post
