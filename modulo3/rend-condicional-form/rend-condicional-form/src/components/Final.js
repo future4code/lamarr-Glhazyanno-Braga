cfd6eb2896158972e833fe17d0a36c288957ce42
Glhazyanno Linhares Braga <glbraga9@gmail.com>
2022-07-12 21:15:53 -0300

    Rederizacao-condicional

---
+++ modulo3/rend-condicional-form/rend-condicional-form/src/components/Final.js
@@ -0,0 +1,11 @@
+import React, {useState} from "react";
+
+export function Final(){
+      return(
+      <div>
+          <h1> O formulario Acabou </h1>
+          <h2> Muito obrigado por participar! Entraremos em contato ! </h2>
+
+      </div>
+)   
+}
