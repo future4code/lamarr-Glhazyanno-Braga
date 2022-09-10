8daf3c83aaff761485df88146108403a864bfe1d
Glhazyanno Linhares Braga <glbraga9@gmail.com>
2022-09-09 22:10:16 -0300

    IabeViagens

--- modulo4/projeto-labex/projeto-labex/src/App.js
+++ modulo4/projeto-labex/projeto-labex/src/App.js
@@ -3,14 +3,10 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route  index element ={Home/>} />
-        <Route path="teams" element />
-
-      </Routes>
+    <>
+ 
       
-    </BrowserRouter>
+    </>
   );
 }
 
